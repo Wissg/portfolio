e199b99db18cf74acd9d6c6290869e47e35d81a7
Wissg <wissal.ghoudi@gmail.com>
2024-05-07 20:02:10 +0200

    imporve particles code

--- src/components/background/particlesContainer.tsx
+++ src/components/background/particlesContainer.tsx
@@ -2,7 +2,10 @@
 
 import React, { useEffect, useMemo, useState } from "react";
 import Particles, { initParticlesEngine } from "@tsparticles/react";
-import { loadSlim } from "@tsparticles/slim";
+// import { loadAll } from "@tsparticles/all"; // if you are going to use `loadAll`, install the "@tsparticles/all" package too.
+// import { loadFull } from "tsparticles"; // if you are going to use `loadFull`, install the "tsparticles" package too.
+import { loadSlim } from "@tsparticles/slim"; // if you are going to use `loadSlim`, install the "@tsparticles/slim" package too.
+// import { loadBasic } from "@tsparticles/basic"; // if you are going to use `loadBasic`, install the "@tsparticles/basic" package too.
 import { particlesConfigDark, particlesConfigLight } from "@/config/particles";
 import { Container, ISourceOptions } from "@tsparticles/engine";
 import { useTheme } from "next-themes";
@@ -28,16 +31,19 @@ const ParticlesContainer: React.FC = () => {
 		setParticlesConfig(getParticleConfig(theme, systemTheme));
 	}, [theme, systemTheme]);
 
+	// this should be run only once per application lifetime
 	useEffect(() => {
-		const initializeParticles = async () => {
-			await initParticlesEngine(async (engine) => {
-				await loadSlim(engine);
-			}).then(() => {
-				setInit(true);
-			});
-		};
-
-		initializeParticles();
+		initParticlesEngine(async (engine) => {
+			// you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+			// this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+			// starting from v2 you can add only the features you need reducing the bundle size
+			//await loadAll(engine);
+			//await loadFull(engine);
+			await loadSlim(engine);
+			//await loadBasic(engine);
+		}).then(() => {
+			setInit(true);
+		});
 	}, []);
 
 	const particlesLoaded = async (container?: Container): Promise<void> => {
