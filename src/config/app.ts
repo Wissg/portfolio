3de66c78abd226e6254eb3c9c4e8b7b3c4adee95
Wissg <wissal.ghoudi@gmail.com>
2024-05-08 15:02:15 +0200

    update config

--- src/config/app.ts
+++ src/config/app.ts
@@ -1,20 +1,13 @@
 export type AppConfig = typeof appConfig;
 
 export const appConfig = {
-	name: "Next.js",
+	name: 'Next.js ds',
 	description:
-		"Beautifully designed components that you can copy and paste into your apps. Accessible. Customizable. Open Source.",
-	mainNav: [
-		{
-			title: "Home",
-			href: "/",
-		},
-	],
+		'Beautifully designed components that you can copy and paste into your apps. Accessible. Customizable. Open Source.',
 	links: {
-		twitter: "https://twitter.com/shadcn",
-		github: "https://github.com/shadcn/ui",
-		docs: "https://ui.shadcn.com",
+		twitter: 'https://twitter.com/wissg',
+		github: 'https://github.com/wissg',
 		paypal:
-			"https://www.paypal.com/paypalme/wghoudi/{amount}?country.x=FR&locale.x=fr_FR",
+			'https://www.paypal.com/paypalme/wghoudi/{amount}?country.x=FR&locale.x=fr_FR',
 	},
 };
