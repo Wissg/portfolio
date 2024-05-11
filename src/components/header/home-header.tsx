abd3cdcabdc086a3a8c465373a6e4bd77fb68b87
Wissg <wissal.ghoudi@gmail.com>
2024-05-11 18:59:33 +0200

    add linkedin link + remove twitter link

--- src/components/header/home-header.tsx
+++ src/components/header/home-header.tsx
@@ -6,38 +6,39 @@ import { buttonVariants } from "@/components/ui/button";
 import { Icons } from "@/components/icons/header-icons";
 import { MainNav } from "@/components/header/main-navBar";
 import { ThemeToggle } from "@/components/theme-toggle";
+import { Linkedin } from 'lucide-react';
 
 export function SiteHeader() {
 	return (
 		<header className="sticky top-0 z-40 w-full border-b bg-background">
 			<div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
 				<MainNav items={navItems} />
-				<div className="flex flex-1 items-center justify-end space-x-4">
-					<nav className="flex items-center space-x-1">
+				<div className="flex flex-1 items-center justify-end">
+					<nav className="flex items-center">
 						<Link
 							href={appConfig.links.github}
 							target="_blank"
 							rel="noreferrer">
 							<div
 								className={buttonVariants({
-									size: "icon",
-									variant: "ghost",
+									size: 'icon',
+									variant: 'ghost',
 								})}>
 								<Icons.gitHub className="h-5 w-5" />
 								<span className="sr-only">GitHub</span>
 							</div>
 						</Link>
 						<Link
-							href={appConfig.links.twitter}
+							href={appConfig.links.linkedin}
 							target="_blank"
 							rel="noreferrer">
 							<div
 								className={buttonVariants({
-									size: "icon",
-									variant: "ghost",
+									size: 'icon',
+									variant: 'ghost',
 								})}>
-								<Icons.twitter className="h-4 w-4 fill-current" />
-								<span className="sr-only">Twitter</span>
+								<Linkedin className="h-4 w-4 fill-current" />
+								<span className="sr-only">Linkedin</span>
 							</div>
 						</Link>
 						<ThemeToggle />
