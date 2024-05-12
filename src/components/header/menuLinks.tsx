078b3200c22d6496d789ead56f2dfc008463f6c2
Wissg <wissal.ghoudi@gmail.com>
2024-05-12 23:33:11 +0200

    revert last change on menu link

--- src/components/header/menuLinks.tsx
+++ src/components/header/menuLinks.tsx
@@ -1,17 +1,12 @@
-'use client';
 import { NavMenuLink } from '@/models/navMenuInterfaces';
 import { NavigationMenuLink } from '@/components/ui/navigation-menu';
-import { useActiveSectionContext } from '@/provider/active-section-context';
-import Link from 'next/link';
-import { motion } from 'framer-motion';
+import { cn } from '@/lib/utils';
 
 interface MenuLinksProps {
 	links: NavMenuLink[];
 }
 
 export function MenuLinks({ links }: MenuLinksProps) {
-	const { activeSection, setActiveSection, setTimeOfLastClick } =
-		useActiveSectionContext();
 	return (
 		<ul
 			className={`grid gap-3 p-4 ${
@@ -29,25 +24,16 @@ export function MenuLinks({ links }: MenuLinksProps) {
 				return (
 					<li key={index}>
 						<NavigationMenuLink asChild>
-							<Link
+							<a
 								className={`block select-none rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground ${disabledClass}`}
-								{...linkProps}
-								onClick={() => {
-									setActiveSection(link.title);
-									setTimeOfLastClick(Date.now());
-								}}>
-								<div
-									className={`${
-										link.title === activeSection
-											? 'text-cyan-500 font-bold '
-											: ''
-									} text-sm font-medium leading-none`}>
+								{...linkProps}>
+								<div className="text-sm font-medium leading-none">
 									{link.title}
 								</div>
 								<p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
 									{link.description}
 								</p>
-							</Link>
+							</a>
 						</NavigationMenuLink>
 					</li>
 				);
