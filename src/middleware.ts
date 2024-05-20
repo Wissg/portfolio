c48797af0685fb4bc42bb1350006d1b47ef91942
Wissg <wissal.ghoudi@gmail.com>
2024-05-20 23:13:21 +0200

    fix middleware (trace request)

---
+++ src/middleware.ts
@@ -0,0 +1,16 @@
+import { getLogger } from '@/utils/logUtils';
+import type { NextRequest } from 'next/server';
+
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+	const correlationId = crypto.randomUUID();
+	request.headers.set('x-correlation-id', correlationId);
+	const logger = getLogger('request').child({ correlationId });
+
+	// Log all request headers and important information
+	logger.debug('Received request', {
+		method: request.method,
+		url: request.url,
+		headers: Object.fromEntries(request.headers.entries()),
+	});
+}
