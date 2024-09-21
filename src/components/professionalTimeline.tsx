b6c80f7a3fd5d5d946797e11a1af0b79d83dd8ef
Wissg <wissal.ghoudi@gmail.com>
2024-09-21 20:09:56 +0200

    (fix timeline component) + better responsiveness

--- src/components/professionalTimeline.tsx
+++ src/components/professionalTimeline.tsx
@@ -9,8 +9,8 @@ import {
 	TimelineLine,
 } from '@/components/ui/timeline';
 
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
-import { Button } from '@/components/ui/button';
+import {Avatar, AvatarFallback, AvatarImage} from '@/components/ui/avatar';
+import {Button} from '@/components/ui/button';
 import {
 	HoverCard,
 	HoverCardContent,
@@ -19,8 +19,8 @@ import {
 
 import SectionHeading from '@/components/commons/section-heading';
 import Link from 'next/link';
-import { CalendarIcon } from 'lucide-react';
-import { Education, Experience } from '@/models/professionalProfileModels';
+import {CalendarIcon} from 'lucide-react';
+import {Education, Experience} from '@/models/professionalProfileModels';
 
 // Define the props interface with a union type for education and experience
 interface ProfessionalTimelineProps {
@@ -30,35 +30,25 @@ interface ProfessionalTimelineProps {
 
 // Define the ProfessionalTimeline functional component
 const ProfessionalTimeline: React.FC<ProfessionalTimelineProps> = ({
-	professional,
-	headingSection,
-}) => {
+																	   professional,
+																	   headingSection,
+																   }) => {
 	return (
 		<>
 			<SectionHeading>{headingSection}</SectionHeading>
-			<Timeline positions="center">
+			<Timeline className="mb-3 last:mb-0 mx-auto max-w-full sm:max-w-2xl lg:max-w-3xl text-center leading-8 scroll-mt-28 px-4 sm:px-6">
 				{professional.map((item, index) => (
 					<TimelineItem key={index} status={'done'}>
-						<TimelineHeading side={index % 2 === 0 ? 'left' : 'right'}>
+						<TimelineHeading side={index % 2 === 0 ? 'right' : 'right'} className="break-words whitespace-normal">
 							{item.title}
 						</TimelineHeading>
-						<TimelineDot status={item.status} />
-						<TimelineLine done />
-						<TimelineContent side={index % 2 === 0 ? 'left' : 'right'}>
-							<span className="block text-sm text-gray-600 mb-2">
-								{item.dateBegin} {item.dateEnd && ' - ' + item.dateEnd}
-							</span>
-							<span className="block text-base text-gray-800">
-								{item.description}
-							</span>
-						</TimelineContent>
-						<TimelineContent
-							side={index % 2 === 1 ? 'left' : 'right'}
-							className="h-full">
-							<div className="flex justify-center items-center h-full">
+						<TimelineDot status={item.status}/>
+						<TimelineLine done/>
+						<TimelineContent side={index % 2 === 0 ? 'right' : 'right'}>
+							<div className="">
 								<HoverCard>
 									<HoverCardTrigger asChild>
-										<Button variant="link">{item.organization.name}</Button>
+										<Button variant="link" className={"px-0"}>{item.organization.name}</Button>
 									</HoverCardTrigger>
 									<HoverCardContent className="w-80 text-left">
 										<div className="flex justify-between space-x-4">
@@ -85,7 +75,7 @@ const ProfessionalTimeline: React.FC<ProfessionalTimelineProps> = ({
 														'No description available.'}
 												</p>
 												<div className="flex items-center pt-2">
-													<CalendarIcon className="mr-2 h-4 w-4 opacity-70" />
+													<CalendarIcon className="mr-2 h-4 w-4 opacity-70"/>
 													<span className="text-xs text-muted-foreground">
 														Joined {item.dateBegin}
 													</span>
@@ -95,7 +85,17 @@ const ProfessionalTimeline: React.FC<ProfessionalTimelineProps> = ({
 									</HoverCardContent>
 								</HoverCard>
 							</div>
+							<div className="grid grid-cols-2 gap-4">
+								<div className=" text-sm text-gray-600 mb-2">
+									{item.dateBegin} {item.dateEnd && ' - ' + item.dateEnd}
+								</div>
+
+							</div>
+							<span className="block text-base text-gray-800">
+								{item.description}
+							</span>
 						</TimelineContent>
+
 					</TimelineItem>
 				))}
 			</Timeline>
--- src/components/ui/timeline.tsx
+++ src/components/ui/timeline.tsx
@@ -1,21 +1,21 @@
-import React from 'react';
-import { VariantProps, cva } from 'class-variance-authority';
-import { Check, Circle, X } from 'lucide-react';
+import React from "react"
+import { VariantProps, cva } from "class-variance-authority"
+import { Check, Circle, X } from "lucide-react"
 
-import { cn } from '@/lib/utils';
+import { cn } from "@/lib/utils"
 
-const timelineVariants = cva('flex flex-col items-stretch', {
+const timelineVariants = cva("grid", {
 	variants: {
 		positions: {
-			left: 'sm:[&>li]:grid-cols-[0_min-content_1fr]',
-			right: 'sm:[&>li]:grid-cols-[1fr_min-content]',
-			center: 'sm:[&>li]:grid-cols-[1fr_min-content_1fr]',
+			left: "[&>li]:grid-cols-[0_min-content_1fr]",
+			right: "[&>li]:grid-cols-[1fr_min-content]",
+			center: "[&>li]:grid-cols-[1fr_min-content_1fr]",
 		},
 	},
 	defaultVariants: {
-		positions: 'left',
+		positions: "left",
 	},
-});
+})
 
 interface TimelineProps
 	extends React.HTMLAttributes<HTMLUListElement>,
@@ -27,25 +27,26 @@ const Timeline = React.forwardRef<HTMLUListElement, TimelineProps>(
 			<ul
 				className={cn(timelineVariants({ positions }), className)}
 				ref={ref}
-				{...props}>
+				{...props}
+			>
 				{children}
 			</ul>
-		);
+		)
 	}
-);
-Timeline.displayName = 'Timeline';
+)
+Timeline.displayName = "Timeline"
 
-const timelineItemVariants = cva('grid items-center gap-x-2', {
+const timelineItemVariants = cva("grid items-center gap-x-2", {
 	variants: {
 		status: {
-			done: 'text-primary',
-			default: 'text-muted-foreground',
+			done: "text-primary",
+			default: "text-muted-foreground",
 		},
 	},
 	defaultVariants: {
-		status: 'default',
+		status: "default",
 	},
-});
+})
 
 interface TimelineItemProps
 	extends React.HTMLAttributes<HTMLLIElement>,
@@ -59,101 +60,101 @@ const TimelineItem = React.forwardRef<HTMLLIElement, TimelineItemProps>(
 			{...props}
 		/>
 	)
-);
-TimelineItem.displayName = 'TimelineItem';
+)
+TimelineItem.displayName = "TimelineItem"
 
 const timelineDotVariants = cva(
-	'col-start-2 col-end-3 row-start-1 row-end-1 flex size-4 items-center justify-center rounded-full border border-current',
+	"col-start-2 col-end-3 row-start-1 row-end-1 flex size-4 items-center justify-center rounded-full border border-current",
 	{
 		variants: {
 			status: {
-				default: '[&>*]:hidden',
+				default: "[&>*]:hidden",
 				current:
-					'[&>*:not(.lucide-circle)]:hidden [&>.lucide-circle]:fill-current [&>.lucide-circle]:text-current',
-				done: 'bg-primary [&>*:not(.lucide-check)]:hidden [&>.lucide-check]:text-background',
+					"[&>*:not(.lucide-circle)]:hidden [&>.lucide-circle]:fill-current [&>.lucide-circle]:text-current",
+				done: "bg-primary [&>*:not(.lucide-check)]:hidden [&>.lucide-check]:text-background",
 				error:
-					'border-destructive bg-destructive [&>*:not(.lucide-x)]:hidden [&>.lucide-x]:text-background',
-				custom: '[&>*:not(:nth-child(4))]:hidden [&>*:nth-child(4)]:block',
+					"border-destructive bg-destructive [&>*:not(.lucide-x)]:hidden [&>.lucide-x]:text-background",
+				custom: "[&>*:not(:nth-child(4))]:hidden [&>*:nth-child(4)]:block",
 			},
 		},
 		defaultVariants: {
-			status: 'default',
+			status: "default",
 		},
 	}
-);
+)
 
 interface TimelineDotProps
 	extends React.HTMLAttributes<HTMLDivElement>,
 		VariantProps<typeof timelineDotVariants> {
-	customIcon?: React.ReactNode;
+	customIcon?: React.ReactNode
 }
 
 const TimelineDot = React.forwardRef<HTMLDivElement, TimelineDotProps>(
 	({ className, status, customIcon, ...props }, ref) => (
 		<div
 			role="status"
-			className={cn('timeline-dot', timelineDotVariants({ status }), className)}
+			className={cn("timeline-dot", timelineDotVariants({ status }), className)}
 			ref={ref}
-			{...props}>
+			{...props}
+		>
 			<Circle className="size-2.5" />
 			<Check className="size-3" />
 			<X className="size-3" />
 			{customIcon}
 		</div>
 	)
-);
-TimelineDot.displayName = 'TimelineDot';
+)
+TimelineDot.displayName = "TimelineDot"
 
 const timelineContentVariants = cva(
-	'row-start-2 row-end-2 pb-8 text-muted-foreground',
+	"row-start-2 row-end-2 pb-8 text-muted-foreground",
 	{
 		variants: {
 			side: {
-				right: 'col-start-3 col-end-4 mr-auto text-left',
-				left: 'col-start-1 col-end-2 ml-auto text-right',
+				right: "col-start-3 col-end-4 mr-auto text-left",
+				left: "col-start-1 col-end-2 ml-auto text-right",
 			},
 		},
 		defaultVariants: {
-			side: 'right',
+			side: "right",
 		},
 	}
-);
+)
 
 interface TimelineContentProps
-	extends React.HTMLAttributes<HTMLParagraphElement>,
+	extends React.HTMLAttributes<HTMLDivElement>,
 		VariantProps<typeof timelineContentVariants> {}
 
-const TimelineContent = React.forwardRef<
-	HTMLParagraphElement,
-	TimelineContentProps
->(({ className, side, ...props }, ref) => (
-	<div
-		className={cn(timelineContentVariants({ side }), className)}
-		ref={ref}
-		{...props}
-	/>
-));
-TimelineContent.displayName = 'TimelineContent';
+const TimelineContent = React.forwardRef<HTMLDivElement, TimelineContentProps>(
+	({ className, side, ...props }, ref) => (
+		<div
+			className={cn(timelineContentVariants({ side }), className)}
+			ref={ref}
+			{...props}
+		/>
+	)
+)
+TimelineContent.displayName = "TimelineContent"
 
 const timelineHeadingVariants = cva(
-	'row-start-1 row-end-1 line-clamp-1 max-w-full truncate',
+	"row-start-1 row-end-1 line-clamp-1 max-w-full truncate",
 	{
 		variants: {
 			side: {
-				right: 'col-start-3 col-end-4 mr-auto text-left',
-				left: 'col-start-1 col-end-2 ml-auto text-right',
+				right: "col-start-3 col-end-4 mr-auto text-left",
+				left: "col-start-1 col-end-2 ml-auto text-right",
 			},
 			variant: {
-				primary: 'text-base font-medium text-primary',
-				secondary: 'text-sm font-light text-muted-foreground',
+				primary: "text-base font-medium text-primary",
+				secondary: "text-sm font-light text-muted-foreground",
 			},
 		},
 		defaultVariants: {
-			side: 'right',
-			variant: 'primary',
+			side: "right",
+			variant: "primary",
 		},
 	}
-);
+)
 
 interface TimelineHeadingProps
 	extends React.HTMLAttributes<HTMLParagraphElement>,
@@ -165,16 +166,16 @@ const TimelineHeading = React.forwardRef<
 >(({ className, side, variant, ...props }, ref) => (
 	<p
 		role="heading"
-		aria-level={variant === 'primary' ? 2 : 3}
+		aria-level={variant === "primary" ? 2 : 3}
 		className={cn(timelineHeadingVariants({ side, variant }), className)}
 		ref={ref}
 		{...props}
 	/>
-));
-TimelineHeading.displayName = 'TimelineHeading';
+))
+TimelineHeading.displayName = "TimelineHeading"
 
 interface TimelineLineProps extends React.HTMLAttributes<HTMLHRElement> {
-	done?: boolean;
+	done?: boolean
 }
 
 const TimelineLine = React.forwardRef<HTMLHRElement, TimelineLineProps>(
@@ -184,17 +185,17 @@ const TimelineLine = React.forwardRef<HTMLHRElement, TimelineLineProps>(
 				role="separator"
 				aria-orientation="vertical"
 				className={cn(
-					'col-start-2 col-end-3 row-start-2 row-end-2 mx-auto flex h-full min-h-16 w-0.5 justify-center rounded-full',
-					done ? 'bg-primary' : 'bg-muted',
+					"col-start-2 col-end-3 row-start-2 row-end-2 mx-auto flex h-full min-h-16 w-0.5 justify-center rounded-full",
+					done ? "bg-primary" : "bg-muted",
 					className
 				)}
 				ref={ref}
 				{...props}
 			/>
-		);
+		)
 	}
-);
-TimelineLine.displayName = 'TimelineLine';
+)
+TimelineLine.displayName = "TimelineLine"
 
 export {
 	Timeline,
@@ -203,4 +204,4 @@ export {
 	TimelineContent,
 	TimelineHeading,
 	TimelineLine,
-};
+}
